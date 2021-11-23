9701c85afd12e096b121c074d0585fe08a667a74
17798-高原 <yuan.gao03@hand-china.com>
2021-11-23 20:33:46 +0800

    ...

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import  { router }  from './router';
 import   store    from './store'
-// import http from './http'
 
 const app = createApp(App);
 app.use(router);
 app.use(store)
+
+//设置全局变量
 // app.config.globalProperties.$http=http
+
 app.mount('#app');
