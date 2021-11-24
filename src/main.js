bdb4087f1869dfb50fcdc895e891c7e94244a877
17798-高原 <yuan.gao03@hand-china.com>
2021-11-24 15:55:08 +0800

    添加ESlint，格式化代码，代码风格使用的Google一套

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import App from './App.vue';
-import  { router }  from './router';
-import   store    from './store'
-import  './util/rem'
+import {router} from './router';
+import store from './store';
+import './util/rem';
 
 const app = createApp(App);
 app.use(router);
-app.use(store)
+app.use(store);
 
-//设置全局变量
+// 设置全局变量
 // app.config.globalProperties.$http=http
 
 app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,38 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import styleImport from "vite-plugin-style-import";
-const { resolve } = require('path')
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import styleImport from 'vite-plugin-style-import';
+const {resolve} = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",//打包路径
+  base: './', // 打包路径
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src') //设置别名src为@
-    }
+      '@': resolve(__dirname, 'src'), // 设置别名src为@
+    },
   },
-  server:{
-    proxy:{
+  server: {
+    proxy: {
       '/api': {
         target: 'http://rap2api.taobao.org/app/mock/294528',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
-    }
+    },
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
   },
   plugins: [
     vue(),
     styleImport({
       libs: [{
-        libraryName: "vant",
+        libraryName: 'vant',
         esModule: true,
         resolveStyle: (name) => `vant/es/${name}/style`,
-      }, ],
+      }],
     }),
   ],
 
-})
+});
