1eafa489a175829d95133c780ccbeaeb7b0db063
17798-高原 <yuan.gao03@hand-china.com>
2021-11-24 16:44:57 +0800

    修改ESlint，格式化代码，代码风格使用的Standard一套

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  'plugins': {
+  plugins: {
     'postcss-pxtorem': {
       rootValue: 37.5,
       // Vant 官方根字体大小是 37.5
       propList: ['*'],
-      selectorBlackList: ['.norem'],
+      selectorBlackList: ['.norem']
       // 过滤掉.norem-开头的class，不进行rem转换
-    },
-  },
-};
+    }
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,28 @@
-import {defineConfig} from 'vite';
-import vue from '@vitejs/plugin-vue';
-import styleImport from 'vite-plugin-style-import';
-const {resolve} = require('path');
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import styleImport from 'vite-plugin-style-import'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './', // 打包路径
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'), // 设置别名src为@
-    },
+      '@': resolve(__dirname, 'src') // 设置别名src为@
+    }
   },
   server: {
     proxy: {
       '/api': {
         target: 'http://rap2api.taobao.org/app/mock/294528',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    assetsDir: 'assets'
   },
   plugins: [
     vue(),
@@ -30,9 +30,9 @@ export default defineConfig({
       libs: [{
         libraryName: 'vant',
         esModule: true,
-        resolveStyle: (name) => `vant/es/${name}/style`,
-      }],
-    }),
-  ],
+        resolveStyle: (name) => `vant/es/${name}/style`
+      }]
+    })
+  ]
 
-});
+})
